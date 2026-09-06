2b56c2371e6871c9952d8391b7706a7471768710
Dakshina Palasamudrum <dakshinapalas@Dakshinas-MacBook-Pro.local>
2026-09-06 11:34:42 -0700

    Initial Spring Boot scaffold from Spring Initializr

---
+++ src/test/java/com/dakshina/llmgateway/LlmGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dakshina.llmgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
